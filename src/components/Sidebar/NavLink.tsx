2f90e4bccb9c27e3bb8df1a1664ce30e352642b5
Phelipe Magalhães <phelipeamagalhaes@gmail.com>
2022-07-01 16:43:38 -0300

    adiciona fluxo de navegação nos links

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,22 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends LinkProps {
   icon: IconType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
